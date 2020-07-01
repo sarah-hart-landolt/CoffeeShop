a116049dddcc6668558dd7306ee65c525e3445cb
Sarah Hart Landolt <sarah.hart.landolt@gmail.com>
2020-06-30 21:48:06 -0500

    first commit

---
+++ CoffeeShop/js-app/main.js
@@ -0,0 +1,41 @@
+﻿const url = "https://localhost:5001/api/beanvariety/";
+
+const button = document.querySelector("#run-button");
+button.addEventListener("click", () => {
+    getAllBeanVarieties()
+        .then(beanVarieties => {
+            console.log(beanVarieties);
+            render(beanVarieties)
+        })
+});
+
+
+function getAllBeanVarieties() {
+    return fetch(url).then(resp => resp.json());}
+
+
+    const contentTarget = document.querySelector(".beansVarietyContainer")
+
+    const render = beanVarietiesToRender => {
+
+        contentTarget.innerHTML = beanVarietiesToRender.map(
+            (bvObject) => {
+
+                return beanVariety(bvObject)
+            }
+        ).join("")
+    }
+
+
+    const beanVariety = (bvObject) => {
+        return `
+    <article class="beanVarieties">
+    <section class="beanVariety">
+        <header class="beanVariety_name">
+            <h1>${bvObject.name}</h1>
+        </header>
+ 
+    </section>
+</article>
+    `
+    }
